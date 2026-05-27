fc4a1130455b15231a31e2c6bf9cd5697f23a5e5
Felipe Ghensev Peres <felipeghensev@gmail.com>
2026-05-27 20:02:27 -0300

    refactor: criando funções reutilizaveis e limpando lógicas de main.ipynb

--- functions/funcoes.py
+++ functions/funcoes.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+# FUNÇÃO - CORRIGIR NOME DE LINHAS 
 def corrigir_coluna(df,
                     coluna,
                     palavra_anomalica:str,
@@ -6,3 +9,36 @@ def corrigir_coluna(df,
     df[coluna] = df[coluna].replace({palavra_anomalica : correcao})
     
     return df
+
+
+
+# FUNÇÃO - REMOVER COLUNA
+def remover_coluna(df,
+                   coluna):
+    
+    return df.drop(columns=coluna)
+
+
+
+# FUNCÃO - REMOVER VALORES NULOS
+def remover_nulos(df):
+    
+    return df.dropna()
+
+
+
+# FUNÇÃO - CONVERTER COLUNA NUMÉRICA
+def converter_para_numerico(df,coluna):
+    
+    df[coluna] = pd.to_numeric(df[coluna],
+                               errors='coerce')
+
+
+    return df
+
+
+
+# FUÇÃO - REMOVER NULOS DE COLUNA ESPECÍFICA
+def remover_nulos_coluna(df,coluna):
+    
+    return df.dropna(subset=[coluna])
