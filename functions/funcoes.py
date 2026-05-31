67cc0a3dd8877d40e94ac2a6ccb8b63b29f48cad
Felipe Ghensev Peres <felipeghensev@gmail.com>
2026-05-30 23:38:50 -0300

    refactor: tornando analises exploratorias em funções reutilizaveis

--- functions/funcoes.py
+++ functions/funcoes.py
@@ -73,3 +73,33 @@ def criar_grafico_barras(dados,
     plt.tight_layout()
     
     plt.show()
+
+
+
+# FUNÇÃO FILTRAR CLIENTES IDEAIS
+def filtrar_clientes_ideais(df,nota_minima):
+    
+    return df[df['Nota (1-100)'] >= nota_minima]
+
+
+
+# FUNÇÃO DISTRIBUIÇÃO DE FAIXA ETÁRIA
+def criar_faixa_etaria(df):
+
+    return pd.cut(df['Idade'],
+                     bins=[18,25,35,45,55,65,100]).value_counts().sort_index()
+
+
+# FUNÇÃO DISTRIBUIÇÃO SALARIAL
+def criar_faixa_salarial(df):
+
+    return pd.cut(df['Salario Anual (R$)'],
+                        bins=[0,30000,60000,90000,120000,
+                        df['Salario Anual (R$)'].max()]).value_counts().sort_index()
+    
+    
+    
+# FUNÇÃO PARA OBTER AS PROFISSÕES DOS CLIENTES IDEAIS
+def obter_profissoes_ci(df):
+    
+    return df['Profissao'].value_counts()
